2160c2238683d6853e5e37e1d43046346d73ce5a
CMRIT-SarveshSirFaculty <sarvesh.ambala@live.com>
2025-03-05 20:41:54 +0530

    Update server.js

--- Unit_III_Code_Part_A/Simple Node Server/server.js
+++ Unit_III_Code_Part_A/Simple Node Server/server.js
@@ -1,1 +1,15 @@
+//Importing the http module
+const http = require("http")
+//Creating Server
+const server = http.createServer((req, res) => {
+    //Sending the response
+    res.write("This is the response from the Server")
+    console.log("This is the response from the Server");
+    res.end();
+})
 
+const host = 'localhost';
+const port = 8000;
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
